244519212f90ed4d0f47961f000bdf4d211f0d4a
BarinovKirill <Barinovkirill1992@gmail.com>
2021-02-11 15:16:37 +0300

    burgrer temp

---
+++ src/js/burger-nav.js
@@ -0,0 +1,24 @@
+const menu = document.querySelector('.nav'),
+    burger = document.querySelector('.burger'),
+    mobileBack = document.querySelector('.mobile-back'),
+    overlay = document.querySelector('.overlay');
+
+    const lockScroll = () => {
+        document.body.classList.add('lock')
+    }
+
+    const unlockScroll = () => {
+        document.body.classList.remove('lock')
+    }
+
+    burger.addEventListener('click', () => {
+        menu.classList.add('open');
+        overlay.classList.add('open');
+        lockScroll();
+    });
+
+    overlay.addEventListener('click', () => {
+        menu.classList.remove('open');
+        overlay.classList.remove('open');
+        lockScroll();
+    });
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import './sticky-header';
 import './hero-slider';
 import './fslightbox';
+import './burger-nav';
